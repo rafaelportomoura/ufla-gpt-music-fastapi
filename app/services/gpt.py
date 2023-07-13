6460b8073cfa062cc8640a55f35afcf4c581f7f5
Vinicius Neiva <viniciustneiva@gmail.com>
2023-07-12 22:32:54 -0300

    [04] Integração com o GPT

--- app/services/gpt.py
+++ app/services/gpt.py
@@ -1,3 +1,4 @@
+import requests
 from typing import List
 from os import environ
 
@@ -7,6 +8,19 @@ KEY = environ["CHATGPT_API_KEY"] if "CHATGPT_API_KEY" in environ else ""
 class Gpt:
     def __init__(self, key: str = KEY) -> None:
         self.key = key
+        self.url = 'https://api.openai.com/v1/chat/completions'
+        self.headers = {"Authorization": f"Bearer {self.key}"}
 
     def suggestions(self, query: str) -> List[str]:
-        return ["Vamos Fugir", "Saudosa Maloca", query]
+        params = {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": query}],
+            "temperature": 0.7
+        }
+        response = requests.post(self.url, params=params, headers=self.headers)
+        result_list = []
+
+        if(response.status_code == 200):
+            result_list = response.json()["choices"][0]["message"]["content"].split(',');
+
+        return result_list
